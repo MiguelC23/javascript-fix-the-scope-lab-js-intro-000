d67f8e12ed978e385c732729e06d6f636c898856
Miguel Carmona <miguelcarmona996@gmail.com>
2019-08-06 02:52:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,60 +1,44 @@
-})
- })
+var animal = 'dog'
 
-   function myAnimal() {
-   var animal = 'dog';
-   return animal;
- }
-
- describe('yourAnimal()', function() {
-   it('returns your animal', function(){
-     expect(window.yourAnimal()).toEqual('cat')
-@@ -17,23 +22,48 @@ describe('Fix the Scope', function() {
-   })
- })
-
- function yourAnimal() {
-   var animal = 'cat';
-   return animal;
- }
-
- describe('add2(n)', function() {
-   it('adds two to n', function() {
-     const n = Math.floor(Math.random() * 1000)
-     expect(window.add2(n)).toEqual(n + 2)
-   })
- })
-
- function add2(n) {
-  var two = 2
-   return n + two;
- }
-
- describe('funkyFunction()', function() {
-   it('returns a function', function() {
-     expect(typeof window.funkyFunction()).toEqual('function')
-   })
- })
-
- var funkyFunction = function() {
-   return function(){
-     return "FUNKY!"
-   }
+function myAnimal() {
+  return animal
 }
 
+function myAnimal() {
+var animal = 'dog';
+return animal;
+}
 
- describe('theFunk', function() {
-   it('is "FUNKY!"', function() {
-     expect(window.theFunk).toEqual('FUNKY!')
-   })
- })
+function yourAnimal() {
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+  return animal
+}
 
- var funkyFunction = function() {
-   return function(){
-     return "FUNKY!"
-   }
- }
+function yourAnimal() {
+  var animal = 'cat';
+  return animal;
+}
 
- var theFunk = funkyFunction()()
+function add2(n) {
+  return n + two
 
-})
+  // Feel free to move things around!
+  const two = 2
+}
+
+function add2(n) {
+ var two = 2
+  return n + two;
+}
+
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
+
+// We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
+var theFunk = funkyFunction
+var theFunk = funkyFunction()()
