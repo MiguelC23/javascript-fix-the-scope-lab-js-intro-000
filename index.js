5e79686458c98b521f8a722ef3749434987f2fdb
Miguel Carmona <miguelcarmona996@gmail.com>
2019-08-06 02:49:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ var funkyFunction = function() {
 // We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
 var theFunk = funkyFunction
 var theFunk = funkyFunction()()
- 30  test/index-test.js 
+ 30  test/index-test.js
 Viewed
 @@ -7,6 +7,11 @@ describe('Fix the Scope', function() {
    })
